9b50907b038b2324195164a1173a2d8d652ad5e2
raphaelstary <raphael.stary@gmail.com>
2015-07-08 23:54:05 +0200

    add https support

---
+++ Config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    SSL_KEY_PATH: 'path/to/key.pem',
+    SSL_CERT_PATH: 'path/to/cert.pem',
+    SSL_PASSPHRASE: 'this is optional',
+
+    PORT: 8088
+};
--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var https = require('https');
+var fs = require('fs');
 
-var PORT = 8088;
-
+var Config = require('./Config');
 var RegistrationHandler = require('./model/RegistrationHandler');
 var AnalyticsHandler = require('./model/AnalyticsHandler');
 var Checker = require('./model/Checker');
@@ -14,7 +15,7 @@ var main = new AnalyticsHandler(checker);
 
 app.use(bodyParser.json());
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
@@ -24,6 +25,12 @@ app.post('/register', register.handle.bind(register));
 
 app.post('/event', main.handle.bind(main));
 
-var server = app.listen(PORT, function () {
+var options = {
+    key: fs.readFileSync(Config.SSL_KEY_PATH, 'utf8'),
+    cert: fs.readFileSync(Config.SSL_CERT_PATH, 'utf8'),
+    passphrase: Config.SSL_PASSPHRASE
+};
+
+var server = https.createServer(options, app).listen(Config.PORT, function () {
     console.log('Holmes started listening on port ' + server.address().port);
 });
